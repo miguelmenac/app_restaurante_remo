ed44c437ae97deb2fa6a1aeaf64cb73712c4ee3a
Miguel Mena Correa <miguelmena@outlook.com>
2019-09-03 20:03:15 -0500

    rutas y postres

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,8 @@ const routes: Routes = [
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
   },
   { path: '', loadChildren: './login/login.module#LoginPageModule' },
-  { path: 'splash-screen', loadChildren: './splash-screen/splash-screen.module#SplashScreenPageModule' }
+  { path: 'splash-screen', loadChildren: './splash-screen/splash-screen.module#SplashScreenPageModule' },
+  { path: 'tables', loadChildren: './tables/tables.module#TablesPageModule' }
 
 ];
 @NgModule({
